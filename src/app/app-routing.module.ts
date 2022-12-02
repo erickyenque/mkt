feec65a4a10ff815b9d11b2658cc9b07314d115c
GladysAZ <gladysabanto99@gmail.com>
2022-12-02 14:03:44 -0500

    perfil, y lista categoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListCategoryComponent } from './components/list-category/list-category.component';
 
 const routes: Routes = [
-    { 
-        path: 'home', 
-        loadChildren: () => 
-            import('./modules/home/home.module').then(m => m.HomeModule)
-    },
-    { path: 'category', component: ListCategoryComponent}
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'perfil',
+    loadChildren: () =>
+      import('./modules/perfil/perfil.module').then((m) => m.PerfilModule),
+  },
+  { path: 'category', component: ListCategoryComponent },
 ];
-  
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}

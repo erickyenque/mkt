533199398dafcc2e98372724397005082a7e5c7c
Erick Yenque Medina <eyenque@canvia.com>
2022-12-02 09:02:25 -0500

    Configuración de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BottomNavigationComponent } from './components/bottom-navigation/bottom-navigation.component';
+import { HomeModule } from './modules/home/home.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ListCategoryComponent } from './components/list-category/list-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    BottomNavigationComponent,
+    ListCategoryComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

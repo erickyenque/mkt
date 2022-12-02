21df0470a31198266fc07d2e501d3604a5310444
Erick Yenque Medina <eyenque@canvia.com>
2022-12-02 09:25:42 -0500

    Modulo perfil

--- src/app/modules/home/components/informative-section/informative-section.component.ts
+++ src/app/modules/home/components/informative-section/informative-section.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informative-section.component.css']
 })
 export class InformativeSectionComponent implements OnInit {
+  
+  img = 'https://pymstatic.com/87428/conversions/elementos-anuncio-publicidad-wide.jpg'
 
   constructor() { }
 
